b367687ab43f034289c55af17fdee7e8832fa6cf
DevGirGoswami <dev025232@gmail.com>
2022-01-19 10:03:02 -0800

    template added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
 function App() {
-  return <div>raj</div>;
+  return (
+    <div className="main-container">
+      <div className="rightsideinput">
+        <div className="flex">
+          <p>From</p>
+          <input type="text" />
+        </div>
+        <div className="flex">
+          <p>To</p>
+          <input type="text" />
+        </div>
+        <div className="flex">
+          <p>Amount</p>
+          <input type="number" />
+        </div>
+        <div className="flex">
+          <p>Descraption</p>
+          <input type="text" />
+        </div>
+        <div className="rightbtn">
+          <button>Send Introduction</button>
+        </div>
+      </div>
+      <div className="leftsideinput">
+        <div className="leftsideheading">
+          <p>Amount Details</p>
+          <div className="leftsidesecond">
+            <p>accountname</p>
+            <input type="text" />
+            <button>SEND</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;

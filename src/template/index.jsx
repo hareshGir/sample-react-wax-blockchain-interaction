31d03d87c14db3158e3ab3983d92c6f2bde19708
Jaipal Puri <jaipalgowami555@gmail.com>
2022-01-25 15:34:57 +0500

    Validation Implementation

--- src/template/index.jsx
+++ src/template/index.jsx
@@ -1,27 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./style.module.css";
 
 const Template = () => {
- return (
+const [inputFrom,setInputFrom]=useState("");
+const [errorFrom,setErrorFrom]=useState();
+const [inputTo,setInputTo]=useState("");
+const [errorTo,setErrorTo]=useState();
+const [inputAmount,setInputAmount]=useState("");
+const [errorInputAmount,setErrorInputAmount]=useState();
+const [inputTA,setInputTA]=useState("");
+const [errorinputTA,setErrorInputTA]=useState();
+
+
+const handleFrom=(e)=>{
+setInputFrom(e.currentTarget.value);
+}
+const handleTo=(e)=>{
+  setInputTo(e.currentTarget.value);
+}
+const handleAmount=(e)=>{
+  setInputAmount(e.currentTarget.value);
+}
+const handleTA=(e)=>{
+  setInputTA(e.currentTarget.value);
+}
+
+
+const handleTransaction=()=>{
+  
+  if(inputFrom.trim()==="")
+  setErrorFrom("From is required");
+  else setErrorFrom("");
+   
+  if(inputTo.trim()==="")
+  setErrorTo("To is required");
+  else setErrorTo("");
+  
+  if(inputAmount.trim()==="")
+  setErrorInputAmount("Amount is required");
+  else setErrorInputAmount("");
+   
+  if(inputTA.trim()==="")
+  setErrorInputTA("Text Area is required");
+  else setErrorInputTA("");
+   
+  }
+
+  return (
+     
   <div className={classes.templateMainContainer}>
    <div className={classes.leftSection}>
     <div>
-     <label>From Account</label>
-     <input type="text" />
+     <label>From </label>
+     <input value ={inputFrom} onChange={handleFrom} autoFocus type="text"  />
     </div>
+    <p>{errorFrom}</p>
     <div>
      <label>To</label>
-     <input type="text" />
+     <input value ={inputTo} onChange={handleTo} type="text" />
     </div>
+    <label>{errorTo}</label>
     <div>
      <label>Amount</label>
-     <input type="number" />
+     <input value ={inputAmount} onChange={handleAmount} type="number" />
     </div>
+    <label>{errorInputAmount}</label>
+    
     <div>
      <label>Description</label>
-     <textarea name="desc" id="desc"></textarea>
+     <textarea value ={inputTA} onChange={handleTA} name="desc" id="desc"></textarea>
     </div>
-    <button>Send Transaction</button>
+    <label>{errorinputTA}</label>
+    <button  onClick={handleTransaction} >Send Transaction</button>
    </div>
    <div className={classes.rightSection}>Right</div>
   </div>

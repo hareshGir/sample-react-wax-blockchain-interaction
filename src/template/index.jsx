3d61b088e2dc6c5d73e4b83839b20077f1c02748
Jaipal Puri <jaipalgowami555@gmail.com>
2022-01-27 23:12:04 +0500

    Check balance functionality and send transaction functionality added

--- src/template/index.jsx
+++ src/template/index.jsx
@@ -3,77 +3,84 @@ import classes from "./style.module.css";
 
 const Template = () => {
 const [inputFrom,setInputFrom]=useState("");
-const [errorFrom,setErrorFrom]=useState();
 const [inputTo,setInputTo]=useState("");
 const [errorTo,setErrorTo]=useState();
 const [inputAmount,setInputAmount]=useState("");
 const [errorInputAmount,setErrorInputAmount]=useState();
 const [inputTA,setInputTA]=useState("");
-const [errorinputTA,setErrorInputTA]=useState();
+const [accountNo,setAccountNo]=useState("IBAN");
+const [balance,setBalance]=useState();
 
+const validate=()=>{
+   
+  if(inputTo==="")
+  setErrorTo("To is required");
+ else setErrorTo ("");
+  
+  if(inputAmount==="")
+  setErrorInputAmount("Amount is required");
+   else setErrorInputAmount("");
+  
+}
+const handleAccountChange=(e)=>{
+  setAccountNo(e.currentTarget.value)
 
-const handleFrom=(e)=>{
-setInputFrom(e.currentTarget.value);
 }
-const handleTo=(e)=>{
-  setInputTo(e.currentTarget.value);
+const checkBalance=()=>{
+  setBalance(50000)
+  setInputFrom(accountNo)
 }
-const handleAmount=(e)=>{
-  setInputAmount(e.currentTarget.value);
-}
-const handleTA=(e)=>{
-  setInputTA(e.currentTarget.value);
-}
-
 
 const handleTransaction=()=>{
-  
-  if(inputFrom.trim()==="")
-  setErrorFrom("From is required");
-  else setErrorFrom("");
-   
-  if(inputTo.trim()==="")
-  setErrorTo("To is required");
-  else setErrorTo("");
-  
-  if(inputAmount.trim()==="")
-  setErrorInputAmount("Amount is required");
-  else setErrorInputAmount("");
-   
-  if(inputTA.trim()==="")
-  setErrorInputTA("Text Area is required");
-  else setErrorInputTA("");
-   
-  }
+  validate();
+  setBalance(balance-inputAmount)
+setInputFrom("")
+setInputTo("")
+setInputAmount("")
+}
 
   return (
-     
   <div className={classes.templateMainContainer}>
    <div className={classes.leftSection}>
     <div>
      <label>From </label>
-     <input value ={inputFrom} onChange={handleFrom} autoFocus type="text"  />
+     <input value ={inputFrom} onChange={(e)=>{
+setInputFrom(e.currentTarget.value);
+}}  type="text"  />
     </div>
-    <p>{errorFrom}</p>
     <div>
      <label>To</label>
-     <input value ={inputTo} onChange={handleTo} type="text" />
+     <input value ={inputTo} onChange={(e)=>{
+  setInputTo(e.currentTarget.value);
+}} type="text" />
     </div>
     <label>{errorTo}</label>
     <div>
      <label>Amount</label>
-     <input value ={inputAmount} onChange={handleAmount} type="number" />
+     <input value ={inputAmount} onChange={(e)=>{
+  setInputAmount(e.currentTarget.value);
+}} type="number" />
     </div>
     <label>{errorInputAmount}</label>
     
     <div>
      <label>Description</label>
-     <textarea value ={inputTA} onChange={handleTA} name="desc" id="desc"></textarea>
+     <textarea value ={inputTA} onChange={(e)=>{ setInputTA(e.currentTarget.value);}}
+      name="desc" id="desc"></textarea>
     </div>
-    <label>{errorinputTA}</label>
-    <button  onClick={handleTransaction} >Send Transaction</button>
+    <button disabled={inputFrom === ""?true:false||inputTo === ""?true:false||inputAmount === ""?true:false} onClick={handleTransaction}  >Send Transaction</button>
    </div>
-   <div className={classes.rightSection}>Right</div>
+   <div className={classes.leftSection}>
+   <label>Account Detail</label>
+     <div>
+     <label  >Account </label>
+     <input autoFocus onChange={handleAccountChange} value={accountNo} type="text" />
+    </div>
+    <button disabled={false} onClick={checkBalance}>Check Balance</button>
+    <br />
+    <div><label>Balance:{balance}</label></div>
+    </div>
+      
   </div>
  );
 };
